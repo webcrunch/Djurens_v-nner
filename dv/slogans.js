8d61951eb8d0292583ef57e2b4b872b767b3d43b
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2015-05-06 18:16:17 +0200

    Lägg till dv

---
+++ dv/slogans.js
@@ -0,0 +1,8 @@
+
+var slogans = [];
+slogans[0] = 'Hälp till att värna om våra vänner djuren.';
+slogans[1] = 'Djuren är varelser som kan känna, glädjas och lida<br /> - liksom vi människor. Liksom du själv.';
+slogans[2] = 'Kom ihåg att husdjuren är dina vänner.Behandla vart och ett av dem som en trofast vän.';;
+slogans[3] = 'Bättre 1 katt i hemmet än 10 på gatan.<br /> Kastrera/sterilisera er katt!';
+slogans[4] = 'Lämna ej din sommarkatt kvar till ett okänt öde.';
+slogans[5] = 'Hjälp oss hjälpa djuren. Bli medlem du också.';
